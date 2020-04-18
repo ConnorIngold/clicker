e33577ba8d3c5c7320e0362ccb8a59345a1675e2
connoringold <connoringold@gmail.com>
2020-04-18 20:06:50 +0100

    Clicker done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const add = () => {
+    setCount(count + 1);
+  }
+  const minus = () => {
+    setCount(count - 1);
+  };
+  const double = () => {
+    setCount(count * 2);
+  };
+  const Half = () => {
+    setCount(count / 2);
+  };
+  const zero = () => {
+    setCount(0);
+  };    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Welcome to the clicker</h1>
+        <div id="amount" className="mt-5">
+          <h3>{count}</h3>
+          <div id="btns">
+            <button className="btn btn-success" onClick={add}>
+              Increment
+            </button>
+            <button className="btn btn-danger" onClick={minus}>
+              Minus
+            </button>
+            <button className="btn btn-info" onClick={double}>
+              Double
+            </button>
+            <button className="btn btn-warning" onClick={Half}>
+              Half
+            </button>
+            <button className="btn btn-secondary" onClick={zero}>
+              Zero
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
